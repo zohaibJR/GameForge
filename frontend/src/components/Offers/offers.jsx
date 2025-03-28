485ece13a7bc173e4fc929c0dc6bdeb36571af5e
zohaibJR <zohaibrehman595@gmail.com>
2025-03-29 03:51:58 +0500

    Added initial Offers Section Duration 0:56:40

---
+++ frontend/src/components/Offers/offers.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './offers.css'
+
+const offers = () => {
+  return (
+    <div className='offers'>
+        <div className='offers-left'>
+            <h1>Exclusive</h1>
+            <h1>Offers For You</h1>
+            <p>Only for Best Seller Products</p>
+            <button>Check Now</button>
+        </div>
+
+        <div className='offers-right'></div>
+
+    </div>
+  )
+}
+
+export default offers
--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
 import Popular from '../components/Popular/popular'
+import Offers from '../components/Offers/offers'
 const Shop = () => {
   return (
     <div>
         <Hero/>
         <Popular />
+        <Offers />
 
     </div>
   )
