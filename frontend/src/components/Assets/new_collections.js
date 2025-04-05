9052546bafa33c62f7a80dc9fa8ef08263d466d7
zohaibJR <zohaibrehman595@gmail.com>
2025-04-05 14:10:20 +0500

    Done with New Colelction Section Dur 1:05:30

---
+++ frontend/src/components/Assets/new_collections.js
@@ -0,0 +1,51 @@
+import fc25 from "../Assets/FC25_PS4.png";
+import minecraft from "../Assets/Minecraft_xBox.png";
+import gta5 from "../Assets/GTA5_PS4.png";
+import w2k23 from "../Assets/W2K23_Xbox.png";
+
+const new_collections = [
+  {
+    id: 1,
+    name: "WWE 2K25",
+    image: fc25,
+    new_price: 12000,
+    old_price: 17000,
+  },
+  {
+    id: 2,
+    name: "NBA 2K25",
+    image: minecraft,
+    new_price: 6500,
+    old_price: 10000,
+  },
+  {
+    id: 3,
+    name: "Prince of Persia",
+    image: gta5,
+    new_price: 5000,
+    old_price: 8000,
+  },
+  {
+    id: 4,
+    name: "Lies of P",
+    image: w2k23,
+    new_price: 12500,
+    old_price: 15000,
+  },
+  {
+    id: 5,
+    name: "Lies of Psas",
+    image: w2k23,
+    new_price: 12500,
+    old_price: 15000,
+  },
+  {
+    id: 6,
+    name: "NBA 2K25",
+    image: minecraft,
+    new_price: 6500,
+    old_price: 10000,
+  },
+];
+
+export default new_collections;
--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from '../components/Hero/Hero'
 import Popular from '../components/Popular/popular'
 import Offers from '../components/Offers/offers'
+import NewCollection from '../components/NewCollection/NewCollection'
 const Shop = () => {
   return (
     <div>
         <Hero/>
         <Popular />
         <Offers />
+        <NewCollection />
 
     </div>
   )
